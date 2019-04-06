d35e0ce27098ab3d3f4e101cf2771a05e56f1f77
Agent Fitz ;-) <fitz.bao@outlook.com>
2019-04-07 01:19:20 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 content = '{"pasgPsnInfo":[{"crdtNo":"110101199004014839","crdtTpCd":"01","operTpCd":"01","pasgPsnChnNm":"张翔","pasgPsnCtcTel":"18889899988","pasgPsnTpCd":"01","pasgTpCd":"A","brthdt":"1990-04-01"}]}';
 var xhr = new XMLHttpRequest();
-xhr.setRequestHeader('ContentType', 'application/json');
 xhr.open('POST','https://www.xiamenair.com/api/members/crm');
+xhr.setRequestHeader('ContentType', 'application/json');
 xhr.send(content);
