01922b3c10cafa5c6354627ad38c4ff920b720c7
Agent Fitz ;-) <fitz.bao@outlook.com>
2019-04-07 02:22:43 +0800

    Update testnew.js

--- testnew.js
+++ testnew.js
@@ -1,4 +1,11 @@
 var xhr = new XMLHttpRequest();
+xhr.onreadystatechange=function()
+  {
+  if (xhr.readyState==4 && xhr.status==200)
+    {
+    console.log(xhr.responseText);
+    }
+  }
 xhr.open('GET','https://www.xiamenair.com/api/members/crm/member?pasgTpCd=01');
 xhr.send();
-alert(xhr.responseText);
+console.log(xhr.responseText);
